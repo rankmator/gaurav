b5074b4bee5eb030418621a11e354464d9e4b1a7
Manish-webx <kushwahamanish956541@gmail.com>
2026-07-16 10:00:29 +0530

    form active

--- includes/header.php
+++ includes/header.php
@@ -22,6 +22,10 @@ $is_services = in_array($cur_script, ['implants.php', 'smile-designing.php', 're
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="img/logo-dr-mathpal.png">
+    <link rel="shortcut icon" href="img/logo-dr-mathpal.png" type="image/x-icon">
+
     <!-- CSS -->
     <link rel="stylesheet" href="css/styles.css?v=<?php echo filemtime('css/styles.css'); ?>">
 
--- nav-links.php
+++ nav-links.php
@@ -3,5 +3,9 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 
+<!-- Favicon -->
+<link rel="icon" type="image/png" href="img/logo-dr-mathpal.png">
+<link rel="shortcut icon" href="img/logo-dr-mathpal.png" type="image/x-icon">
+
 <!-- CSS -->
 <link rel="stylesheet" href="css/styles.css?v=<?php echo filemtime('css/styles.css'); ?>">
